2ffe2770e038a25c9b2cb8a0552b9e969eacde00
Wayne Griffiths <wayne.c.griffiths@gmail.com>
2025-05-26 08:42:35 +0800

    feat: Enum  Rule

---
+++ src/rules/EnumRule.ts
@@ -0,0 +1,18 @@
+import { BaseRule } from './BaseRule.js';
+
+export class EnumRule extends BaseRule<string[]> {
+  validate(input: string): boolean {
+    return this.value.includes(input);
+  }
+
+  fix(_input: string): string | null {
+    // Can't automatically fix enum issues
+    // Could potentially use string similarity to find closest match,
+    // but that's beyond the scope of a simple fix
+    return null;
+  }
+
+  errorMessage(): string {
+    return `be one of: ${this.value.join(', ')}`;
+  }
+}
