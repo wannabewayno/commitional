49b3b9f7bf98e516a3642f144f66b411d991c0de
Wayne Griffiths <wayne.c.griffiths@gmail.com>
2025-06-08 10:11:15 +0800

    feat: Allow empty input as valid in EnumRule validation
    Allow empty input to be considered valid in EnumRule validation to support scenarios where input might be optional.
    
    This change assumes that if input is required, it will be enforced by separate rules such as allow-empty rules. This prevents false negatives during validation when the input is empty.

--- src/rules/EnumRule.ts
+++ src/rules/EnumRule.ts
@@ -2,6 +2,9 @@ import { BaseRuleWithValue } from './BaseRule.js';
 
 export class EnumRule extends BaseRuleWithValue<string[]> {
   validate(input: string): boolean {
+    // Can't validate empty input, asssume it's valid
+    // If input is required it will be picked up by allow-empty rules.
+    if (!input) return true;
     return this.value.includes(input);
   }
 
