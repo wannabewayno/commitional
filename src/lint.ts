bb1a930d9ac418cd4ebcc6e1c5408fd53ef6a87f
Wayne Griffiths <wayne.c.griffiths@gmail.com>
2025-07-03 23:19:05 +0800

    feat: Add basic commit message linting setup [w.i.p]
    Implement basic linting for commit messages by reading the commit message from a given file path and parsing it into a CommitMessage object. This setup lays the groundwork for further validation and interactive editing of commit messages to enforce commit conventions.

--- src/lint.ts
+++ src/lint.ts
@@ -1,18 +1,21 @@
+import { readFile } from 'node:fs/promises';
+import CommitMessage from './CommitMessage/index.js';
+
+interface LintOpts {
+  edit?: boolean;
+}
 
 /**
  * Lints a commit message
  * By default takes the latest commit message or
  */
-export default () => {
-  // takes one argument the path to the commit message.
+export default async (commitMsgPath: string, _opts: LintOpts) => {
+  const commit = await readFile(commitMsgPath, 'utf-8').then(CommitMessage.fromString);
+  console.log(commit);
 
   // load the commit message unless a flag was parsed to get some other commit message.
 
   // parse current rules.
 
-  // parse the commit.
-
-  // for each commit part evaluate the rules.
-
   // show the rules to user and start a TTY shell with commitional to edit the message. and save it back to the temporary file.
-}
+};
